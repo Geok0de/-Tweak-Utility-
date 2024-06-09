4ae3b2b819bbf2e9a1b68bdaa785651f4e0b832b
L22k0de <34183211+L22k0de@users.noreply.github.com>
2024-06-09 19:21:59 +0900

    Nvidia AIO
    엔비디아 AIO

---
+++ TooLazy.bat
@@ -0,0 +1,35 @@
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_Video_Active_Device" /t REG_DWORD /d "2147483648" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Brightness" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Brightness" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_RGB_Gamma_B" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_RGB_Gamma_B" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Saturation" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Saturation" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Contrast" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Contrast" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_RGB_Gamma_R" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_RGB_Gamma_R" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XALG_Color_Range" /t REG_BINARY /d "0000000000000000" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Color_Range" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_RGB_Gamma_G" /t REG_DWORD /d "100" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_RGB_Gamma_G" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Hue" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Hue" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Edge_Enhance" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Noise_Reduce" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_VAL_Edge_Enhance" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Noise_Reduce" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XALG_Cadence" /t REG_BINARY /d "0400000000000000" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XEN_Cadence" /t REG_DWORD /d "2147483649" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XALG_Noise_Reduce" /t REG_BINARY /d "0000000000000000" /f
+Reg.exe add "HKLM\System\ControlSet001\Control\Class\{4d36e968-e325-11ce-bfc1-08002be10318}\0000" /v "_User_SUB0_DFP2_XALG_Edge_Enhance" /t REG_BINARY /d "0000000000000000" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473410" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473411" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473412" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473413" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473414" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473415" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473416" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473417" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "3473418" /t REG_DWORD /d "100" /f
+Reg.exe add "HKCU\SOFTWARE\NVIDIA Corporation\Global\NVTweak\Devices\344251435-0\Color" /v "NvCplUseColorCorrection" /t REG_DWORD /d "1" /f
